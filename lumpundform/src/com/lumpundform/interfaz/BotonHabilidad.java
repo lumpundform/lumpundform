a146cc6564ff0962459d4cbf828d32611142ea2f
Sergio Valencia <sevaor@gmail.com>
2012-12-12 20:12:08 -0700

    Los enemigos caminan en dirección del héroe

--- lumpundform/src/com/lumpundform/interfaz/BotonHabilidad.java
+++ lumpundform/src/com/lumpundform/interfaz/BotonHabilidad.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.lumpundform.actores.Heroe;
 import com.lumpundform.escenario.EscenarioBase;
-import com.lumpundform.excepciones.EscenarioSinHeroeException;
 import com.lumpundform.excepciones.HabilidadInexistenteException;
 import com.lumpundform.habilidades.Habilidad;
 import com.lumpundform.utilerias.U;
@@ -17,8 +16,7 @@ class BotonHabilidad extends BotonSuperior {
 	private Habilidad habilidad;
 
 	BotonHabilidad(EscenarioBase escenario, Habilidad habilidad, int posicion) {
-		super(new NinePatch(new Texture(Gdx.files.internal("manabar.png")), 1,
-				1, 1, 1), escenario);
+		super(new NinePatch(new Texture(Gdx.files.internal("manabar.png")), 1, 1, 1, 1), escenario);
 
 		this.habilidad = habilidad;
 
@@ -30,14 +28,11 @@ class BotonHabilidad extends BotonSuperior {
 		addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				BotonHabilidad boton = (BotonHabilidad) event
-						.getListenerActor();
+				BotonHabilidad boton = (BotonHabilidad) event.getListenerActor();
+				Heroe heroe = ((EscenarioBase) boton.getStage()).getHeroe();
 				try {
-					Heroe heroe = ((EscenarioBase) boton.getStage()).getHeroe();
 					heroe.habilidad(boton.habilidad.getNombre());
 				} catch (HabilidadInexistenteException e) {
-					U.err(e);
-				} catch (EscenarioSinHeroeException e) {
 					U.err(e);
 				}
 			}
@@ -46,9 +41,7 @@ class BotonHabilidad extends BotonSuperior {
 
 	private float xHabilidad(int posicion) {
 		if (posicion > (UI.cantHabilidades / 2)) {
-			return UI.margen
-					+ ((getCamara().viewportWidth - (2 * UI.margen)))
-					- anchoBoton()
+			return UI.margen + ((getCamara().viewportWidth - (2 * UI.margen))) - anchoBoton()
 					- ((UI.cantHabilidades - posicion) * (anchoBoton() + UI.margen));
 		} else {
 			return UI.margen + ((posicion - 1) * (anchoBoton() + UI.margen));
