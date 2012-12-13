7bfcfb7953db97d6ad30a50e47066adbe1c305ab
Sergio Valencia <sevaor@gmail.com>
2012-12-12 20:12:08 -0700

    Los enemigos caminan en dirección del héroe

--- lumpundform/src/com/lumpundform/escenario/EscenarioHelper.java
+++ lumpundform/src/com/lumpundform/escenario/EscenarioHelper.java
@@ -89,7 +89,7 @@ public class EscenarioHelper {
 
 		// Eventos
 		escenario.revisarEventos(camara, delta);
-		//escenario.revisarEscena();
+		// escenario.revisarEscena();
 		escenario.destruirAtaques(camara);
 
 		escenario.acomodarActores(mh.getWidth());
