07b678ca50f236571b3e794dc5605d11dfa0c860
Sergio Valencia <sevaor@gmail.com>
2013-01-31 09:05:32 -0700

    Quitar HEADs de comentarios

--- lumpundform/src/com/lumpundform/acciones/HeroeAction.java
+++ lumpundform/src/com/lumpundform/acciones/HeroeAction.java
@@ -5,15 +5,9 @@ import com.lumpundform.actores.Heroe;
 
 /**
  * Las acciones que realiza el {@link Heroe} cada vez que se manda llamar
-<<<<<<< HEAD
  * {@link Heroe#act(float)}.
  * 
  * @author Sergio Valencia
-=======
- * {@link Heroe#act(float)}
- * 
- * @author sergio
->>>>>>> 388e50ad9cb0558a3b2e54f4c5e01fee93a0ea40
  * 
  */
 public class HeroeAction extends Action {
