167f87295941eefacb263878d24d4ef03e0cdacb
Sergio Valencia <sevaor@gmail.com>
2013-02-11 16:41:26 -0700

    Quitar import no usado

--- lumpundform/src/com/lumpundform/escenario/EscenarioBase.java
+++ lumpundform/src/com/lumpundform/escenario/EscenarioBase.java
@@ -15,7 +15,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.AlphaAction;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.XmlReader;
